9a36cceb890762946c0c670aa2c5825ab2232498
Taisei <taisei96.web@gmail.com>
2023-06-06 13:39:19 +0900

    README記入

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ const App = () => {
   const onClickCountUp = () => {
     setNum(num + 1);
   }
+
+  {/* 配列の分割代入でuseStateから取り出す。num = stateで使用する変数名　setNum = stateを更新していく関数名 
+      numを動的にsetNumで更新していく
+  */}
   const [num, setNum] = useState(0);
 
   return (
@@ -13,7 +17,7 @@ const App = () => {
       <p>{num}</p>
     </>
   )
-  
+
 }
 
 export default App
